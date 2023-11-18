f5822d9163f4f022f0be3d5093153f07cd3f8f63
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2023-11-18 17:16:50 +0100

    large display for ["Créer un projet", "Modifier un projet", "Créer une alternance", "Modifier une alternance"]

--- backOffice/src/components/Drawer.tsx
+++ backOffice/src/components/Drawer.tsx
@@ -14,12 +14,13 @@ import { ProjetType } from '../types/ProjetType';
 import UpdateProjet from './UpdateProjet';
 const DrawerItem = () => {
     const { drawerOpen, closeDrawer, drawerDisplay, drawerData } = useContext(DrawerContext);
+    const largeDisplay = ["Créer un projet", "Modifier un projet", "Créer une alternance", "Modifier une alternance"]
     return (
         <Drawer title={<h3 className='dark:text-white'>{drawerDisplay}</h3>} closeIcon={
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-x dark:text-white" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
             </svg>}
-            placement="right" onClose={closeDrawer} open={drawerOpen} className=' dark:bg-bgDark' size={drawerDisplay === "Créer un projet" || drawerDisplay === "Modifier un projet" ? "large" : "default"}>
+            placement="right" onClose={closeDrawer} open={drawerOpen} className=' dark:bg-bgDark' size={largeDisplay.includes(drawerDisplay) ? "large" : "default"}>
             {drawerDisplay === "Créer une expérience" && <CreateExperience />}
             {drawerDisplay === "Modifier une expérience" && <UpdateExperience experience={drawerData as ExperienceType} />}
             {drawerDisplay === "Créer une compétence" && <CreateCompetence />}
